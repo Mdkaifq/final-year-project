028c1a8cac9a130d1ac31d8345622ddd388aee4c
Mdkaifq <140621920+Mdkaifq@users.noreply.github.com>
2024-05-15 07:40:13 +0530

    Update views.py

--- risk_calc/views.py
+++ risk_calc/views.py
@@ -7,8 +7,6 @@ from django.views.decorators.csrf import csrf_exempt
 def risk_calc(request):
     value_list = ['Age', 'Genetic Risk', 'Occupational Hazards', 'Headache',	'Seizure Rate',	'Convulsion Rate',	'Blurred Vision',	'Double Vision',	'Peripheral Vision Loss',	'Loss of Smell',	'Balance Disorder',	'Nausea',	'Vomiting',	'Rate of Memory Lapses',	'Drowsiness',	'Personality disorder',	'Numbness in arms',	'Numbness in legs',	'Syncope Rate'] # it will change every time view1 will get request
     pred = ['no risk detected', 'risk detected']
-    #df = pd.DataFrame(columns=value_list)
-    #df.columns=value_list
     df = None
     result = None
     if request.method == "POST":
@@ -16,8 +14,5 @@ def risk_calc(request):
         #print(values_from_user)
         df = pd.DataFrame([values_from_user], columns=value_list)
         prediction = predict(df)
-        print(type(prediction))
         result = pred[prediction[0]]
-        #return render(request, 'index.html', {'result': result})
-        print(df)
     return render (request, 'riskcalc.html', {'value_list': value_list, 'result':result})
