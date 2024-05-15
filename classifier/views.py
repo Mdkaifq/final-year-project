1f72dd1ab2cfc68cf2a620957f14fc861e8017fb
Mdkaifq <140621920+Mdkaifq@users.noreply.github.com>
2024-05-15 07:38:06 +0530

    Update views.py

--- classifier/views.py
+++ classifier/views.py
@@ -8,10 +8,10 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import os
 
-# Load the pre-trained model
+
 model = load_model('/content/final-year-project/templates/cnnbtfinal_newest.h5')
 
-# Mapping for class labels
+
 class_labels = {0: 'glioma',
             1: 'meningioma',
             2: 'no tumor',
@@ -30,21 +30,12 @@ def classify_image(request):
                 for chunk in img1.chunks():
                     temp_file.write(chunk)
             img = cv2.imread(temp_image_path)
-            #img = cv2.bilateralFilter(img, 2, 50, 50)
-            #img = cv2.applyColorMap(img, cv2.COLORMAP_BONE)
             img = cv2.resize(img, (150, 150))
             img_array = np.array(img)
-            #img_array = np.expand_dims(img_array, axis=0)
-            #img_array = img_array / 255.0  # Normalize
             img_array = img_array.reshape(1,150,150,3)
             a=model.predict(img_array)
             indices = a.argmax()
             os.remove(temp_image_path)
             result = class_labels[indices]
-           
-
-        
-            # Print the error for debugging
-            
 
     return render(request, 'imgclassifier.html', {'result': result})
